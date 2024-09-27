5e81596c79b1a71b63c8903413cb69f52c84b384
tayyabaNoreen1 <tayyaba1.noreen@gmail.com>
2024-09-27 16:40:37 +0100

    mocha javascript first three videos

---
+++ test/mochaHooks.js
@@ -0,0 +1,24 @@
+describe('Mocha Hooks', function () {
+
+    before('Execute Before All Tests', function () {
+        console.log('Execute Before All Tests');
+    });
+
+    beforeEach('Execute Before Each Test', function () {
+        console.log('Execute Before Each Test');
+    });
+
+    after('Execute After All Tests', function () {
+        console.log('Execute After All Tests');
+    });
+
+    afterEach('Execute After Each Test', function () {
+        console.log('Execute After Each Test');
+    });
+
+    it('Mocha Hooks Test', function () {
+        console.log('Mocha - This is a Test for Mocha Hooks');
+    })
+})
+
+//Use this to run tests for a particular file: npm test test\mochaHooks.js
